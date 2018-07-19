6ac3b8c21fe16f3ea49122a0dadb4efa49a819e2
Edminsson <axel.aguilar@gmail.com>
2018-07-19 13:52:25 +0200

    2614 Making modifications to Ranges

--- Digirati.IIIF3.Tests/ExampleJsonGeneration.cs
+++ Digirati.IIIF3.Tests/ExampleJsonGeneration.cs
@@ -260,5 +260,158 @@ namespace Digirati.IIIF3.Tests
             }
 
         }
+
+
+        [Test]
+        public void TestIiif3RangeExample()
+        {
+            Manifest manifest = new Manifest();
+            manifest.Label = new JSONLDString("en", "Book 1");
+            manifest.Context = Defaults.Context;
+            manifest.Id = "https://example.org/iiif/book1/manifest";
+            manifest.ViewingDirection = "right-to-left";
+            manifest.Behavior = new List<string>() { "paged" };
+            manifest.NavDate = "1856-01-01T00:00:00Z";
+            manifest.Rights = "https://creativecommons.org/licenses/by/4.0/";
+            manifest.RequiredStatement = new Metadata
+            {
+                Label = new JSONLDString("en", "Attribution"),
+                Value = new JSONLDString("en", "Provided by Example Organization")
+            };
+
+            manifest.Items = new List<Canvas>
+            {
+                new Canvas
+                {
+                    Id = "https://example.org/iiif/book1/canvas/p1",
+                    Label = "p. 1",
+                    Height = 1000,
+                    Width = 750,
+                    Items = new List<AnnotationPage>
+                    {
+                        new AnnotationPage
+                        {
+                            Id = "https://example.org/iiif/book1/page/p1/1",
+                            Items = new List<Annotation>
+                            {
+                                new Annotation
+                                {
+                                    Id = "https://example.org/iiif/book1/annotation/p0001-image",
+                                    Motivation = "painting",
+                                    Body = new ImageResource("https://example.org/iiif/book1/res/page1.jpg")
+                                    {
+                                        Format = "image/jpeg",
+                                        Service = new List<Service>
+                                        {
+                                            new Service
+                                            {
+                                                Id = "https://example.org/images/book1-page1",
+                                                Type = "ImageService3",
+                                                Profile = "level2"
+                                            }
+                                        },
+                                        Width = 1500,
+                                        Height = 2000
+                                    },
+                                    Target = "https://example.org/iiif/book1/canvas/p1"
+                                }
+                            }
+                        }
+                    }
+                },
+                new Canvas
+                {
+                    Id = "https://example.org/iiif/book1/canvas/p2",
+                    Label = "p. 2",
+                    Height = 1000,
+                    Width = 750,
+                    Items = new List<AnnotationPage>
+                    {
+                        new AnnotationPage
+                        {
+                            Id = "https://example.org/iiif/book1/page/p2/1",
+                            Items = new List<Annotation>
+                            {
+                                new Annotation
+                                {
+                                    Id = "https://example.org/iiif/book1/annotation/p0002-image",
+                                    Motivation = "painting",
+                                    Body = new ImageResource("https://example.org/iiif/book1/res/page1.jpg")
+                                    {
+                                        Format = "image/jpeg",
+                                        Service = new List<Service>
+                                        {
+                                            new Service
+                                            {
+                                                Id = "https://example.org/images/book1-page2",
+                                                Type = "ImageService3",
+                                                Profile = "level2"
+                                            }
+                                        },
+                                        Width = 1500,
+                                        Height = 2000
+                                    },
+                                    Target = "https://example.org/iiif/book1/canvas/p2"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var rangeLabelEnglishSwedish = new JSONLDString(new Dictionary<string, List<string>>
+            {
+                { "en", new List<string> { "Table of Content" } },
+                { "sv", new List<string> { "Innehållsförteckning"} }
+            });
+            manifest.Structures = new List<Range>
+            {
+                new Range
+                {
+                    Id = "https://example.org/iiif/book1/range/r0",
+                    Type = "Range",
+                    Label = rangeLabelEnglishSwedish,
+                    Items = new List<Range>
+                    {
+                        new Range {
+                            Id ="https://example.org/iiif/book1/range/r1",
+                            Type = "Range",
+                            Label = new JSONLDString("bild1"),
+                            Items = new List<Range>
+                            {
+                                new Range
+                                {
+                                    Id = "https://example.org/iiif/book1/canvas/p1",
+                                    Type = "Canvas",
+                                }
+                            }
+                        },
+                        new Range
+                        {
+                            Id ="https://example.org/iiif/book1/range/r2",
+                            Type = "Range",
+                            Label = new JSONLDString("bild2"),
+                            Items = new List<Range>
+                            {
+                                new Range
+                                {
+                                    Id = "https://example.org/iiif/book1/canvas/p2",
+                                    Type = "Canvas",
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            using (var sw = new StringWriter())
+            {
+                JsonSerializer.Create(Defaults.JsonSerializerSettings).Serialize(sw, manifest);
+                Console.Write(sw.ToString());
+            }
+
+        }
+
+
     }
 }
